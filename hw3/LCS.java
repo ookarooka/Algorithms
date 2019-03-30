f5339750cb5dee35a1658fac601cd238dd4e2c51
Josh Patterson <34358565+ookarooka@users.noreply.github.com>
2019-03-29 18:51:16 -0700

    10/10 new topdown

--- hw3/LCS.java
+++ hw3/LCS.java
@@ -1,6 +1,7 @@
 package lcs;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayList;
 
 public class LCS {
 
@@ -63,36 +64,45 @@ public class LCS {
    // Top-Down LCS
    // -----------------------------------------------
 
-    public static Set<String> topDownLCS (String rStr, String cStr) {
-    HashSet<int[]> hasVisited = new HashSet<>();
-    int[][] memoTable = new int[rStr.length() + 1][cStr.length() + 1];
-       memoCheck = memoTable;
-    topDownTableFill(rStr, rStr.length(), cStr, cStr.length(), memoCheck, hasVisited);
-    return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
-    }
-
-    static void topDownTableFill(String rStr, int r, String cStr, int  c, int[][] memo, HashSet<int[]> hasVisited) {
-       int[] coords = {rStr.length(), cStr.length()};
-       if (hasVisited.contains(coords)) {
-         return;
-       }
-       else {
-          hasVisited.add(coords);
-       }
-       //base
-       if (r == 0 || c == 0) {
- 			     return;
- 		  }
-       ///recur1
-       if (rStr.charAt(r - 1) == cStr.charAt(c - 1)) {
-         topDownTableFill(rStr, rStr.length()-1, cStr, cStr.length()-1, memoCheck, hasVisited);
-       }
-       else {
-         //recur2
-         topDownTableFill(rStr, rStr.length()-1, cStr, cStr.length(), memoCheck, hasVisited);
-         //recur3
-         topDownTableFill(rStr, rStr.length(), cStr, cStr.length()-1, memoCheck, hasVisited);
-       }
-       return;
-     }
-}
+	 public static Set<String> topDownLCS (String rStr, String cStr) {
+	    	ArrayList<String> visited = new ArrayList<>();
+	    	memoCheck = topDownTableFill(rStr, cStr, new int[rStr.length()+1][cStr.length()+1], visited);
+	    	return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
+	    }
+	        
+	    /**
+	     * Fills the memoization table using top-down dynamic programming
+	     * @param rStr The String found along the table's rows
+	     * @param cStr The String found along the table's cols
+	     * @param visited ArrayList of Strings for which the sub-solution has already been found (stored as "rStr|cStr")
+	     * @return 2D array containing the memoized values
+	     */
+	 
+	 private static String removeLastChar(String str) {
+		    return str.substring(0, str.length() - 1);
+		}
+	 
+	 public static char lastChar(String s) {
+	    	return s.charAt(s.length()-1);
+	    }
+	 
+	    public static int[][] topDownTableFill( String rStr, String cStr, int[][] inputTable, ArrayList<String> visited) {
+	    	int[][] memoTable = inputTable;
+	    	
+	    	if ( visited.contains(rStr + "|" + cStr) ) {
+	    		return memoTable;
+	    	}
+	    	
+	    	if ( rStr.length() == 0 || cStr.length() == 0 ) {
+	    		memoTable[rStr.length()][cStr.length()] = 0;
+	    	} else if ( lastChar(rStr) == lastChar(cStr) ) {
+	    		memoTable[rStr.length()][cStr.length()] = 1 + topDownTableFill( removeLastChar(rStr), removeLastChar(cStr), memoTable, visited)[rStr.length()-1][cStr.length()-1];
+	    	} else {
+	    		memoTable[rStr.length()][cStr.length()] = Math.max( topDownTableFill( removeLastChar(rStr), cStr, memoTable, visited)[rStr.length()-1][cStr.length()], topDownTableFill( rStr, removeLastChar(cStr), memoTable, visited)[rStr.length()][cStr.length()-1]);
+	    	}
+	    	
+	    	visited.add(rStr + "|" + cStr);
+	    	return memoTable;
+	    }
+	    
+	}
