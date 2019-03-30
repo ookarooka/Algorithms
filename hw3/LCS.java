9ca845331575bffcd1e5ff512cfc91104b6e088f
Josh Patterson <34358565+ookarooka@users.noreply.github.com>
2019-03-29 18:35:35 -0700

    7/10 tests passed, need multiple letters

--- hw3/LCS.java
+++ hw3/LCS.java
@@ -18,13 +18,14 @@ public class LCS {
 			return empty;
 		  }
       ///recur1
-      if (rStr.charAt(r - 1) == cStr.charAt(c - 1)) {
-        Set<String> result = collectSolution(rStr, r - 1, cStr, c - 1, memo);
-        for(String temp : result) {
-          temp = temp + "" + rStr.charAt(r-1);
-        }
-        return result;
-      }
+     	if (rStr.charAt(r - 1) == cStr.charAt(c - 1)) {
+    		Set<String> continuing = collectSolution(rStr, r - 1, cStr, c - 1, memo);
+    		Set<String> result = new HashSet<String>();
+    		for (String s : continuing) {
+    			result.add(s + rStr.charAt(r - 1));
+    		}
+    		return result;
+    	}
 
       ///recur2
      	Set<String> merge = new HashSet<String>();
@@ -38,60 +39,60 @@ public class LCS {
      	  return merge;
      }
 
+
     // -----------------------------------------------
     // Bottom-Up LCS
     // -----------------------------------------------
 
-
-     public static Set<String> bottomUpLCS (String rStr, String cStr) {
-         int[][] memoTable = new int[rStr.length() + 1][cStr.length() + 1];
-         memoCheck = memoTable;
-         for(int r = 1; r < rStr.length() + 1; r++) {
-             for(int c = 1; c < cStr.length() + 1; c++) {
-                 if( rStr.charAt(r-1) == cStr.charAt(c-1) ) {
-                    memoTable[r][c] = 1 + memoTable[r-1][c-1];
-                 }
-                 else {
-                    memoTable[r][c]= Math.max( memoTable[r][c-1] , memoTable[r-1][c] );
-                 }
-             }
-         }
-         return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
-     }
-    // -----------------------------------------------
-    // Top-Down LCS
-    // -----------------------------------------------
-
-     public static Set<String> topDownLCS (String rStr, String cStr) {
-     HashSet<int[]> hasVisited = new HashSet<>();
-     int[][] memoTable = new int[rStr.length() + 1][cStr.length() + 1];
+	public static Set<String> bottomUpLCS (String rStr, String cStr) {
+        int[][] memoTable = new int[rStr.length() + 1][cStr.length() + 1];
         memoCheck = memoTable;
-     topDownTableFill(rStr, rStr.length(), cStr, cStr.length(), memoCheck, hasVisited);
-     return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
-     }
+        for(int r = 1; r < rStr.length() + 1; r++) {
+            for(int c = 1; c < cStr.length() + 1; c++) {
+                if( rStr.charAt(r-1) == cStr.charAt(c-1) ) {
+                   memoTable[r][c] = 1 + memoTable[r-1][c-1];
+                }
+                else {
+                   memoTable[r][c]= Math.max( memoTable[r][c-1] , memoTable[r-1][c] );
+                }
+            }
+        }
+        return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
+    }
+   // -----------------------------------------------
+   // Top-Down LCS
+   // -----------------------------------------------
 
-     static void topDownTableFill(String rStr, int r, String cStr, int  c, int[][] memo, HashSet<int[]> hasVisited) {
-        int[] coords = {rStr.length(), cStr.length()};
-        if (hasVisited.contains(coords)) {
-          return;
-        }
-        else {
-           hasVisited.add(coords);
-        }
-        //base
-        if (r == 0 || c == 0) {
-  			     return;
-  		  }
-        ///recur1
-        if (rStr.charAt(r - 1) == cStr.charAt(c - 1)) {
-          topDownTableFill(rStr, rStr.length()-1, cStr, cStr.length()-1, memoCheck, hasVisited);
-        }
-        else {
-          //recur2
-          topDownTableFill(rStr, rStr.length()-1, cStr, cStr.length(), memoCheck, hasVisited);
-          //recur3
-          topDownTableFill(rStr, rStr.length(), cStr, cStr.length()-1, memoCheck, hasVisited);
-        }
-        return;
-      }
+    public static Set<String> topDownLCS (String rStr, String cStr) {
+    HashSet<int[]> hasVisited = new HashSet<>();
+    int[][] memoTable = new int[rStr.length() + 1][cStr.length() + 1];
+       memoCheck = memoTable;
+    topDownTableFill(rStr, rStr.length(), cStr, cStr.length(), memoCheck, hasVisited);
+    return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
+    }
+
+    static void topDownTableFill(String rStr, int r, String cStr, int  c, int[][] memo, HashSet<int[]> hasVisited) {
+       int[] coords = {rStr.length(), cStr.length()};
+       if (hasVisited.contains(coords)) {
+         return;
+       }
+       else {
+          hasVisited.add(coords);
+       }
+       //base
+       if (r == 0 || c == 0) {
+ 			     return;
+ 		  }
+       ///recur1
+       if (rStr.charAt(r - 1) == cStr.charAt(c - 1)) {
+         topDownTableFill(rStr, rStr.length()-1, cStr, cStr.length()-1, memoCheck, hasVisited);
+       }
+       else {
+         //recur2
+         topDownTableFill(rStr, rStr.length()-1, cStr, cStr.length(), memoCheck, hasVisited);
+         //recur3
+         topDownTableFill(rStr, rStr.length(), cStr, cStr.length()-1, memoCheck, hasVisited);
+       }
+       return;
+     }
 }
